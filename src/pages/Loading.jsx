19212b5c58132af0a8a521efdd2fb3847934cce4
Priyanshu1112 <ashu1112sing@gmail.com>
2024-05-03 18:33:28 +0530

    loading screen

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -15,13 +15,13 @@ const Loading = () => {
   }, []);
 
   return (
-    <div className="bg-slate-950 text-white h-screen w-screen flex items-center justify-center text-2xl">
-      <span className="grid grid-cols-2 items-center gap-3 justify-center">
-        <img src="/logo.png" className="h-[6vmax] ml-auto" />
+    <div className="bg-slate-950 text-white h-screen w-screen flex items-center justify-center text-sm md:text-lg lg:text-2xl">
+      <div className="flex flex-col items-center justify-center ">
+        {/* <img src="/logo.png" className="h-[3vmax] md:h-[4vmax] lg:h-[6vmax]" /> */}
         <TextTransition springConfig={presets.wobbly}>
           {TEXTS[index % TEXTS.length]}
         </TextTransition>
-      </span>
+      </div>
     </div>
   );
 };
